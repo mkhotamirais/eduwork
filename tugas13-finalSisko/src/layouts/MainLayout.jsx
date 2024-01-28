e527fb8adb68ec53aab2b67101647e1f3da858af
mkhotamirais <mkhotamirais@gmail.com>
2024-01-28 19:24:26 +0700

    finalisasi

--- tugas13-finalSisko/src/layouts/MainLayout.jsx
+++ tugas13-finalSisko/src/layouts/MainLayout.jsx
@@ -6,7 +6,7 @@ const MainLayout = () => {
   return (
     <>
       <Header />
-      <main className="min-h-[calc(100vh-6rem)] px-3 lg:px-16 bg-gradient-to-br from-red-50 via-green-100 to-blue-50">
+      <main className="min-h-[calc(100vh-6rem)] px-3 pb-12 lg:px-16 bg-gradient-to-br from-red-50 via-green-100 to-blue-50">
         <Outlet />
       </main>
       <Footer />
