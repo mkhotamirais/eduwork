f99fc955ba3e482d2cdaf6374353243f41f0d7fd
mkhotamirais <mkhotamirais@gmail.com>
2024-02-05 03:59:05 +0700

    clean up pertama migrasi ke mern-eduwork

--- tugas14-ujianKelulusan/src/layouts/Header.jsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <header className="z-50 h-16 border-b px-3 lg:px-16 flex justify-center items-center sticky top-0 bg-white">
-      header
-    </header>
-  );
-};
-
-export default Header;
